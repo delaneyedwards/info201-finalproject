9db409b1151dae2e4848660219fc07b0792ea998
ayun6 <ayun6@uw.edu>
2020-11-11 19:41:39 +0800

    table

---
+++ scripts/table.R
@@ -0,0 +1,83 @@
+library("tidyverse")
+
+states_all_extended <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-kellenmcgibbon/gh-pages/data/states_all_extended.csv?token=ARHGKDWWYSBRXYTE5XSXDXC7WMYOE")
+states_all <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-kellenmcgibbon/gh-pages/data/states_all.csv?token=ARHGKDQRPCKSQUYR2XV3VRK7WMYSA")
+
+avg_scores <- states_all %>%
+  filter(YEAR == 2015) %>%
+  select(STATE, 
+         AVG_READING_4_SCORE,
+         AVG_READING_8_SCORE)
+
+scores_by_race<- states_all_extended %>%
+  filter(YEAR == 2015) %>%
+  group_by(STATE) %>%
+  select(STATE, 
+         INSTRUCTION_EXPENDITURE, 
+         G04_WH_A_READING,
+         G04_BL_A_READING,
+         G04_HI_A_READING,
+         G04_AS_A_READING,
+         G04_AM_A_READING,
+         G04_HP_A_READING,
+         G04_TR_A_READING,
+         G08_WH_A_READING,
+         G08_BL_A_READING,
+         G08_HI_A_READING,
+         G08_AS_A_READING,
+         G08_AM_A_READING,
+         G08_HP_A_READING,
+         G08_TR_A_READING) %>%
+  rename(WHITE_GR04_READING = G04_WH_A_READING) %>%
+  rename(BLACK_GR04_READING = G04_BL_A_READING) %>% 
+  rename(LATINX_GR04_READING = G04_HI_A_READING) %>% 
+  rename(ASIAN_GR04_READING = G04_AS_A_READING) %>% 
+  rename(AMERICAN_INDIAN_ALASKA_NATIVE_GR04_READING = G04_AM_A_READING) %>% 
+  rename(HAWAIIAN_NATIVE_PACIFIC_ISLANDER_GR04_READING = G04_HP_A_READING) %>% 
+  rename(TWO_OR_MORE_RACES_GR04_READING = G04_TR_A_READING) %>%
+  rename(WHITE_GR08_READING = G08_WH_A_READING) %>% 
+  rename(BLACK_GR08_READING = G08_BL_A_READING) %>% 
+  rename(LATINX_GR08_READING= G08_HI_A_READING) %>% 
+  rename(ASIAN_GR08_READING = G08_AS_A_READING) %>% 
+  rename(AMERICAN_INDIAN_ALASKA_NATIVE_GR08_READING = G08_AM_A_READING) %>% 
+  rename(HAWAIIAN_NATIVE_PACIFIC_ISLANDER_GR08_READING = G08_HP_A_READING) %>% 
+  rename(TWO_OR_MORE_RACES_GR08_READING = G08_TR_A_READING) 
+
+states_table <- left_join(scores_by_race, avg_scores, by = "STATE") 
+  
+states_table <- states_table[-52, ]
+
+states_table <- mutate(
+  states_table, 
+  WHITE_DIFF_FROM_AVG = mean((WHITE_GR04_READING - AVG_READING_4_SCORE) +
+                                (WHITE_GR08_READING - AVG_READING_8_SCORE)),
+  BLACK_DIFF_FROM_AVG = mean((BLACK_GR04_READING - AVG_READING_4_SCORE) +
+                                    (BLACK_GR08_READING - AVG_READING_8_SCORE)),
+  LATINX_DIFF_FROM_AVG = mean((LATINX_GR04_READING - AVG_READING_4_SCORE) -
+                                (LATINX_GR08_READING - AVG_READING_8_SCORE)),
+  ASIAN_DIFF_FROM_AVG = mean((ASIAN_GR04_READING - AVG_READING_4_SCORE) + 
+                               (ASIAN_GR08_READING - AVG_READING_8_SCORE)),
+  AMERICAN_INDIAN_ALASKA_NATIVE_DIFF_FROM_AVG = mean(
+    (AMERICAN_INDIAN_ALASKA_NATIVE_GR04_READING - AVG_READING_4_SCORE) + 
+     (AMERICAN_INDIAN_ALASKA_NATIVE_GR08_READING - AVG_READING_8_SCORE)),
+  HAWAIIAN_NATIVE_PACIFIC_ISLANDER_DIFF_FROM_AVG = mean(
+    (HAWAIIAN_NATIVE_PACIFIC_ISLANDER_GR04_READING - AVG_READING_4_SCORE) +
+    (HAWAIIAN_NATIVE_PACIFIC_ISLANDER_GR08_READING - AVG_READING_8_SCORE)),
+  TWO_OR_MORE_RACES_DIFF_FROM_AVG = mean((TWO_OR_MORE_RACES_GR04_READING - 
+    AVG_READING_4_SCORE) + (TWO_OR_MORE_RACES_GR08_READING - 
+      AVG_READING_8_SCORE))
+) %>%
+  select(STATE,
+         INSTRUCTION_EXPENDITURE,
+         WHITE_DIFF_FROM_AVG,
+         BLACK_DIFF_FROM_AVG,
+         LATINX_DIFF_FROM_AVG,
+         ASIAN_DIFF_FROM_AVG,
+         AMERICAN_INDIAN_ALASKA_NATIVE_DIFF_FROM_AVG,
+         HAWAIIAN_NATIVE_PACIFIC_ISLANDER_DIFF_FROM_AVG,
+         TWO_OR_MORE_RACES_DIFF_FROM_AVG
+         ) 
+
+ 
+
+
