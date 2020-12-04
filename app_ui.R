a7c73650e7427151e7833fb868fd40de623565b6
ayun6 <ayun6@uw.edu>
2020-12-04 11:08:12 +0800

    intro

--- app_ui.R
+++ app_ui.R
@@ -9,7 +9,36 @@ ap <- read.csv("data/Advanced\ Placement.csv")
 ib <- read.csv("data/International\ Baccalaureate.csv")
 
 intro_page <- tabPanel(
-  "Introduction Page"
+  "Introduction Page",
+  p("Education and academia are what drive the innovations of our world. 
+  In a country where success tends to equate to wealth, education is necessary 
+  to achieve this success and is highly valued. However, equity in student 
+  education can be compromised due to many surrounding variables. Our project 
+  explores the subject of education in the United States in the 2010s. We 
+  focused our efforts on finding discrepancies in students’ performances based 
+  variables such as demographics and government funding."),
+  
+  p("Some questions we were trying to answer included investigating the effect 
+  of state education spendings on the scores for student standadized exams 
+  (NAEP), studying the correlation between student race and their achievements,
+  as well as examining the demographics of students in advanced classes."),
+  
+  p("The dataset we used to answer these questions was from the US Census Bureau
+  and the National Center for Education Statistics, which detailed information 
+  on education in different states including number of students enrolled in 
+  certain grades, the revenue and expenditure of states, average testing scores
+  and demographic data. We found this dataset to be particularly helpful in 
+  answering our questions on how race and state spendings on education impact
+  student achievement as they contain demographic and performance statistics."),
+  
+  p("Another dataset we utilized was from the U.S. Department of Education,
+  which was collected through the Civil Rights Data Collection by the Department
+  of Education Office for Civil Rights. The data here was collected through
+  self-reports by public schools and public school districts, relying on the 
+  schools' and districts' own data gathering methods. Information here 
+  encompasses a comprehensive report of school and school district 
+  characteristics, early childhood education, pathways to college and career,
+  etc.")
 )
 
 bar_chart_page <- tabPanel(
